aaedb10ab19cb53dda2fbc9c6d975fdc3336496e
Sagar Singh <sagar.01ko@gmail.com>
2023-12-28 02:55:53 +0530

    Updated "Post" component

--- src/Components/pages/Product.jsx
+++ src/Components/pages/Product.jsx
@@ -1,53 +1,68 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { MdOutlineNavigateNext } from 'react-icons/md';
+import { GrFormPrevious } from 'react-icons/gr';
+import { current } from '@reduxjs/toolkit';
+import { useState } from 'react';
 
 function Product() {
-    const products = useSelector((state) => state.product.products); // Corrected the path to products
+    const [currentLength, setCurrentLength] = useState(0);
+    const products = useSelector((state) => state.product.products);
     const { id } = useParams();
 
-    const selectedProduct = products?products.find((product) => product.id == id):null;
+    const selectedProduct = products ? products.find((product) => product.id == id) : null;
+
     if (!selectedProduct) {
         // Handle the case where the product is not found
-        return <div>Product not found!</div>;
+        return <div className="text-center mt-10">Product not found!</div>;
     }
 
+    const NextHandler = () => {
+        if (selectedProduct.images && currentLength < selectedProduct.images.length-1) {
+            setCurrentLength(currentLength + 1);
+        }else{
+            setCurrentLength(0);
+        }
+    };
+
+    const prevhandler = () => {
+        if (currentLength > 0) {
+            setCurrentLength(currentLength - 1);
+        }else{
+            setCurrentLength(selectedProduct.images.length-1);
+        }
+    };
+
     return (
-        <>
-            {selectedProduct && (
-                <div key={selectedProduct.id} className="group relative">
-                    <div className="relative aspect-h-1 aspect-w-1 w-full rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                        <img
-                            src={selectedProduct.thumbnail}
-                            alt="thumbnail"
-                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                        />
-                        <div className="absolute top-3 left-[-10px] bg-blue-600 p-2 rounded-e-md">
-                            {Math.floor(selectedProduct.discountPercentage)}% discount
-                        </div>
-                    </div>
-                    <div className="mt-4 flex justify-between">
-                        <div>
-                            <h3 className="text-md font-semibold">
-                                <a href="/">
-                                    <span aria-hidden="true" className="absolute inset-0" />
-                                    {selectedProduct.title}
-                                    <p
-                                        className={`text-sm font-medium my-1 ${selectedProduct.stock > 0 ? 'text-green-600' : 'text-red-500'
-                                            }`}
-                                    >
-                                        {selectedProduct.stock > 0 ? 'In Stock' : 'Out of Stock'}
-                                    </p>
-                                </a>
-                            </h3>
-                            <h2 className="text-sm text-gray-700">{selectedProduct.description}</h2>
-                            <p className="mt-1 text-sm text-gray-500">Rating: {selectedProduct.rating} ⭐</p>
-                        </div>
-                        <p className="text-sm font-medium text-gray-900">${selectedProduct.price}</p>
-                    </div>
+        <div className="flex justify-center items-center mt-[-60px] h-screen overflow-hidden">
+            <div className="flex justify-between w-3/4">
+                <div className="w-1/2 relative overflow-hidden">
+                    <img
+                        src={selectedProduct.images[currentLength]}
+                        alt="thumbnail"
+                        className="rounded-lg shadow-md"
+                    />
+                    <GrFormPrevious onClick={prevhandler} className="cursor-pointer absolute top-[45%] text-blue-700 text-3xl left-5" />
+                    <MdOutlineNavigateNext onClick={NextHandler} className="cursor-pointer absolute top-[45%] text-blue-700 text-3xl right-5" />
                 </div>
-            )}
-        </>
+                <div className="w-1/2 p-8">
+                    <h1 className="text-3xl font-bold mb-2">{selectedProduct.title}</h1>
+                    <p className="text-xl font-semibold text-gray-900">${selectedProduct.price}</p>
+                    <p className="text-md text-gray-700 mt-2">{selectedProduct.description}</p>
+                    <p className="text-sm text-gray-500 mt-2">Rating: {selectedProduct.rating} ⭐</p>
+                    <p
+                        className={`text-sm font-medium my-1 ${selectedProduct.stock > 0 ? 'text-green-600' : 'text-red-500'
+                            }`}
+                    >
+                        {selectedProduct.stock > 0
+                            ? `${selectedProduct.stock} In Stock`
+                            : 'Out of Stock'}
+                    </p>
+                    <button className='px-3 float-end bg-blue-600 rounded-lg text-white font-semibold py-1' type="button">Add to cart</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
